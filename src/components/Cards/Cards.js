f46dc220fdaa98f4e0b1fc74db3c43d62448e03e
Nitish kumar <krnitish540@gmail.com>
2023-09-30 21:43:09 +0530

    Food Cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,10 +1,28 @@
 import React from "react";
-
-const  Cards = () =>{
+import './Cards.css'
+const  Cards = ({img,title,discription,lunch,dinner,breakfast,miniPrice,regPrice,jumboPrice}) =>{
     return(
         <div>
-           <div className=''>
-
+           <div className='menu-card'>
+                  <img src={img} alt="" className="food-img" />
+                 
+                 <h3 className="food-title">{title}</h3>
+                 <p className="food-discription">{discription}</p>
+                 <p className="food-time"><span>{lunch}</span>/<span>{dinner}</span>/<span>{breakfast}</span></p>
+                 <div className="food-flex">
+                      <div className="sub">
+                        <p className="food-price">Mini Meal</p>
+                        <span>₹{miniPrice}</span>
+                      </div>
+                      <div className="sub">
+                        <p className="food-price">Regular Meal</p>
+                        <span>₹{regPrice}</span>
+                      </div>
+                      <div className="sub">
+                        <p className="food-price">jumbo Meal</p>
+                        <span>₹{jumboPrice}</span>
+                      </div>
+                 </div>
            </div>
         </div>
     )
