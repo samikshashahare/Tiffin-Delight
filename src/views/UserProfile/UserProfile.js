02330cb355aadceecb4bc01b1628fa295d8ea7cf
kashishInamdar <kashishinamdar2004@gmail.com>
2023-10-03 10:22:46 +0530

    change Them of Profile Page Pink to Oreng

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -1,8 +1,7 @@
 import React from "react";
-import thali from './thali.jpg'
 import ProfileCard from "../../ProfileCard/ProfileCard";
 import Kashishimg from "./kashish.png"
-import SideBG from "./bgImg.png" 
+import SideBG from "./side-bg.png" 
 
 import "./UserProfile.css"
 const UserProfile = () => {
@@ -14,14 +13,12 @@ const UserProfile = () => {
                 <h1 className="text-center heading "> Profile </h1>
                     <ProfileCard img={Kashishimg} no={"8237988102"} add={"Shivajinagar"} alt="kashish" />
                 </div>
-
                 <div className="col ">
                     <div className="profile-side-templet">
                     <img src={SideBG} className="side-img" alt="img"/>
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
