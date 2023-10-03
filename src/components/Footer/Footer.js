75b91e4109922c73455398786b69c880cf12e15f
sagar-wandile <wandilesagar2926@gmail.com>
2023-10-03 08:43:08 +0530

    Change in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,42 @@
 import React from "react";
-
+import'./Footer.css';
 const  Footer = () =>{
     return(
-        <div>
-            Footer
-        </div>
+       <>
+       <div className="main-container-footer">
+           <div className="footer-list-cont">
+
+              <ul>
+              <h3>Title</h3>
+                <ul>sagar</ul>
+                <ul>akash</ul>
+                <ul>Nihal</ul>
+              </ul>
+              
+              <ol>
+              <h3>Title</h3>
+                <ul>sagar</ul>
+                <ul>akash</ul>
+                <ul>Nihal</ul>
+              </ol>
+              
+              <ol>
+              <h3>Title</h3>
+                <ul>sagar</ul>
+                <ul>akash</ul>
+                <ul>Nihal</ul>
+              </ol>
+
+              <ol>
+              <h3>Title</h3>
+                <ul>sagar</ul>
+                <ul>akash</ul>
+                <ul>Nihal</ul>
+              </ol>
+
+           </div>
+       </div>
+       </>
     )
 }
 
