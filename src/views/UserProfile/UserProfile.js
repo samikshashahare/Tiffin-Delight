2ef1debbdc055079e2106208fa5bfc09351b0448
kashishInamdar <kashishinamdar2004@gmail.com>
2023-10-04 01:25:43 +0530

    Add responsive Media Query in Profile page

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -10,10 +10,10 @@ const UserProfile = () => {
             
             <div className="row ">
                 <div className="col bg-pink">
-                <h1 className="text-center heading "> Profile </h1>
+                <h1 className="text-center heading "> My Profile </h1>
                     <ProfileCard img={Kashishimg} no={"8237988102"} add={"Shivajinagar"} alt="kashish" />
                 </div>
-                <div className="col ">
+                <div className="col remove">
                     <div className="profile-side-templet">
                     <img src={SideBG} className="side-img" alt="img"/>
                     </div>
