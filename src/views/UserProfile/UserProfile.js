e1a24ad572fc0f740143b9e03a607ed5b18985f3
kashishInamdar <kashishinamdar2004@gmail.com>
2023-10-06 05:13:07 +0530

    thid commit is only see my work on vercel

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -4,7 +4,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import ProfileCard from "../../ProfileCard/ProfileCard";
 // import Kashishimg from "./kashish.png" 
 import profileLogo from "./profile-logo.png"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 
 import "./UserProfile.css"
 const UserProfile = () => {
@@ -17,15 +17,22 @@ const UserProfile = () => {
         console.log(profileDetails)
     }, [])
 
+   
+
     return (
         <>
             <Navbar />
 
         <div className="Profile-page" >
+
+            
             
             <div className="row m-0 p-0 w-100 ">
                 <div className="col ">
                 <h1 className="text-center heading "> My Profile </h1>
+
+                <img src="./profile-logo.png" alt="image" />
+                <input type="file" />
                    {
                     getinfo.map((user,index)=>{
                        return <ProfileCard key={index} 
