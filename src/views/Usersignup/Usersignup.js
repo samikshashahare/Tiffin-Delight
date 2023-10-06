fd74f8ce603d7b57fd8c3665cfe907d826c1ee07
Nitish kumar <krnitish540@gmail.com>
2023-10-06 19:09:13 +0530

    Change in css

--- src/views/Usersignup/Usersignup.js
+++ src/views/Usersignup/Usersignup.js
@@ -119,89 +119,87 @@ function Signup() {
       <div className='col-lg-12 col-md-12 col-sm-12 col- pb-2 back-ground-signup'>
 
         <div className='sign-up-form'>
-          <h1 className='mb-2'>jjj</h1>
-          <form className='form form-div'>
+          <h1 className=''>jjj</h1>
+          <form className='input-data mx-auto m-0'>
             <h1 className=' mb-2 ms-5 text-center text-dark' style={{ fontFamily: 'gabriola', fontWeight: 'bolder'}}>Sign Up</h1>
 
 
             <div className="col-md-6 mx-auto col-lg-6 col- col-sm " >
-              <div className="form-floating input mb-3">
+              <div className="input-box">
                 <input
-                  className=' form-control my-input  rounded-2'
                   type="email"
                   value={email}
-                  placeholder='Enter Email'
                   onChange={(e) => setEmail(e.target.value)}
                 />
-                <label htmlFor="floatingInput">Enter Email </label>
+               <span>email</span>
               </div>
             </div>
 
             <div className='col-md-6 mx-auto col-lg-6 col- col-sm'>
-              <div className="form-floating  input mb-3">
+              <div className="input-box">
                 <input
-                  className='form-control my-input rounded-2'
+                 
                   type="Text"
-                  placeholder="Full Name"
+                 
                   value={firstName}
                   onChange={(e) => { setFirstName(e.target.value) }}
                 />
-                <label htmlFor="floatingInput">Full Name</label>
+                <span>Full Name</span>
               </div>
             </div>
 
             <div className='col-md-6 mx-auto col-lg-6 col- col-sm'>
-              <div className="form-floating mb-3 input emoji-position">
+              <div className="input-box input emoji-position">
                 <input
-                  className='form-control my-input rounded-2'
+                 
                   type="Text"
-                  placeholder="Full Address"
+                
                   value={address}
                   onChange={(e) => { setAddress(e.target.value) }}
                 />
 
-                <label htmlFor="floatingInput">Full Address</label>
-                <span onClick={fetchLocation} className='location-emoji'>📍</span>
+                <span>Full Address</span>
+                <span onClick={fetchLocation} style={{position:'absolute', bottom:'-25px', left : '225px', fontSize:'30px'}}>📍</span>
               </div>
             </div>
             <div className='col-md-6 mx-auto col-lg-6 col- col-sm'>
-              <div className="form-floating input mb-3">
+              <div className="input-box">
                 <input
-                  className='form-control my-input rounded-2'
+                
                   type="text"
-                  placeholder="Mobile No."
+                 
                   value={mobile}
                   onChange={(e) => { setMobile(e.target.value) }}
                 />
-                <label htmlFor="floatingInput">Mobile No.</label>
+                <span>Mobile No.</span>
               </div>
             </div>
             <div className="col-md-6 mx-auto col-lg-6 col- col-sm">
-              <div className="form-floating input mb-3">
+              <div className="input-box">
                 <input
-                  className='form-control my-input rounded-2'
+                 
                   type="password"
                   value={password}
-                  placeholder='Password'
+                
                   onChange={(e) => setPassword(e.target.value)}
                 />
-                <label htmlFor="floatingInput">Password</label>
+                <span >Password</span>
               </div>
             </div>
             <div className="col-md-6 mx-auto col-lg-6 col- col-sm">
-              <div className="form-floating input mb-3">
+              <div className="input-box">
                 <input
-                  className='form-control my-input rounded-2'
+                  className=''
                   type="password"
                   value={confirmPassword}
-                  placeholder='ConfirmPassword'
+                 
                   onChange={(e) => setConfirmPassword(e.target.value)}
                 />
-                <label htmlFor="floatingInput">ConfirmPassword</label>
+                <span>ConfirmPassword</span>
               </div>
             </div>
 
-            <button onClick={handleSignup} className='sing-up-btn' type='button'>Sign Up</button>
+            <button onClick={handleSignup} className='send  mx-auto d-block mt-5' type='button'>Sign Up</button>
             <br />
             <Link to={'/login'}  ><span className='d-block text-center p-0 text-black'>Already Account?</span></Link>
 
